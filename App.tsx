b3c75590f1ab6ec88767c0d4ca1b7c0d4cc5d4a8
Sam Bartram <99421933+SdcBartram@users.noreply.github.com>
2023-07-12 10:32:52 +0100

    bottom tab changes complete

--- App.tsx
+++ App.tsx
@@ -2,7 +2,9 @@ import { SafeAreaView, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { useState } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
 // Import Screens
 import ChallengeDashBoardScreen from './screens/ChallengeDashboardScreen';
@@ -18,7 +20,7 @@ export default function App() {
 const Stack = createNativeStackNavigator();
 
   //create bottom tab
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 
 // States
@@ -64,22 +66,51 @@ function Home(){
 
   return (
     <>
-      <SafeAreaView style={styles.container}>
+      <SafeAreaView style={styles.container} >
         <NavigationContainer>
           {/* //stack.navigator - initial screen = login
           //stack.screen
           //check if user
           //if yes take to home */}
           {/* <HomeScreen/> */}
-            <Tab.Navigator
-              initialRouteName="Home"
-              activeColor="#edf6ed"
-              inactiveColor="#3E2465"
-              barStyle={{ backgroundColor: '#694FAD' }}>
-                <Tab.Screen name="HO" component={Home}/>
-                <Tab.Screen name="DC" component={GamesHub}/>
-                <Tab.Screen name="HT" component={Habits}/>
-                <Tab.Screen name="SG" component={Pockets}/>
+            <Tab.Navigator>
+                <Tab.Screen name="HO" component={Home} options={{
+                  headerShown: false,
+                  tabBarIcon: ({ color, size }) => (<AntDesign name="barschart" color="pink" size={50} />),
+                  tabBarActiveTintColor: 'pink',
+                  tabBarInactiveTintColor: 'purple',
+                  tabBarIconStyle: { fontSize: 80 }
+                }}/>
+                <Tab.Screen name="DC" component={GamesHub} options={{
+                  headerShown: false,
+                  tabBarLabel: 'QUIZ',
+                  tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons name="gamepad" color={color} size={size} />
+                  ),
+                  tabBarActiveTintColor: 'orange',
+                  tabBarInactiveTintColor: 'pink', 
+                  // tabBarIconStyle: { fontSize: 40 }
+                }}/>
+                <Tab.Screen name="HT" component={Habits} options={{
+                  headerShown: false,
+                  tabBarLabel: 'CHART',
+                  tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons name="rocket" color={color} size={size} />
+                  ),
+                  tabBarActiveTintColor: 'purple',
+                  tabBarInactiveTintColor: 'green', 
+                  // tabBarIconStyle: { fontSize: 40 }
+                }}/>
+                <Tab.Screen name="SG" component={Pockets} options={{
+                  headerShown: false,
+                  tabBarLabel: 'LIST',
+                  tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons name="shopping" color={color} size={size}  />
+                  ),
+                  tabBarActiveTintColor: 'red',
+                  tabBarInactiveTintColor: 'orange', 
+                  // tabBarIconStyle: { fontSize: 40 }
+                }}/>
             </Tab.Navigator>
           {/* //if no take to login page
           //end of stack navigator */}
