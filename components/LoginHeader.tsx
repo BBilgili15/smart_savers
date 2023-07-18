6a843d8a2d7d68c1ab9cfe936ed37ff330113f6c
Sam Bartram <99421933+SdcBartram@users.noreply.github.com>
2023-07-18 13:56:13 +0100

    fixed raccoon added loginHeader

---
+++ components/LoginHeader.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+
+type LoginHeaderProps = {};
+
+const LoginHeader: React.FC<LoginHeaderProps> = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerLeft}>
+        <Image style={styles.imageLogo} source={require('../images/pig.png')} />
+        <Text style={styles.text}>SMART SAVERS LOGIN</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: 125,
+    backgroundColor: '#ffffff',
+    shadowColor: '#716f6f',
+    shadowOffset: { width: 3, height: 3 },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    borderRadius: 20,
+    marginBottom:30,
+  },
+  text: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#35d0ba',
+    alignSelf: 'center',
+  },
+  imageLogo: {
+    height: 70,
+    width: 70,
+    marginRight: 0,
+  },
+  headerLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderBottomRightRadius: 20,
+    borderTopRightRadius: 20,
+  },
+});
+
+export default LoginHeader;
